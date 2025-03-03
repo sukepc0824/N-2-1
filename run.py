ff895adcdf347c1b9865fa9903749ebe9817fe4d
pcsuke <110383951+sukepc0824@users.noreply.github.com>
2025-03-03 18:49:24 +0900

    Update run.py

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import subprocess
 import time
 import random
 
-def mseive(number,timeout):
+def mseive(number,timeout,core):
     try:
-        command = ['msieve153.exe', '-t', '12' ,'-v' ,'-e', '-q' ,str(number)]
+        command = ['msieve153.exe', '-t', str(core) ,'-v' ,'-e', '-q' ,str(number)]
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         
         try:
@@ -19,23 +19,19 @@ def mseive(number,timeout):
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
-def run(dig,t,timeout):
+def run(dig,t,timeout,core):
     print("Finding Start")
     print(f"Take up to {t*timeout}sec")
     for _ in range(t):
         i = random.randrange(10**(dig-1), 10**(dig))
         n = ((i*2)*(i*2))+1
-        result = mseive(n,timeout)
+        result = mseive(n,timeout,core)
         if len(result.split('\n')) == 2:
             print(f"Found N={i} {result.split('\n')[1]}")
     print("Finding Finish")
-
-print("Digits:")
-D = int(input())
-
-print("Trials:")
-T = int(input())
-
-print("TimeOut(sec):")
-timeout = int(input())
-run(D,T,timeout)
+    
+D = int(input("Digits:"))
+T = int(input("Trials:"))
+timeout = int(input("TimeOut(sec):"))
+core = int(input("UseCores:"))
+run(D,T,timeout,core)
